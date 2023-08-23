b40bc7a2acca5d70b109c80b8d53399da81dfc1b
webdevxru <webdevx@mail.ru>
2023-08-23 16:19:14 +0300

    added fixed header

--- gulpfile.js
+++ gulpfile.js
@@ -19,7 +19,7 @@ function browsersync() {
 
 function styles() {
   return src("app/scss/style.scss")
-    .pipe(scss({ outputStyle: "expanded" }))
+    .pipe(scss({ outputStyle: "compressed" }))
     .pipe(concat("style.min.css"))
     .pipe(
       autoprefixer({
