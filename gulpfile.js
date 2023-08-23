5cc08e8cb8a3dbd2aea80e28703d6d52e6fd1a69
webdevxru <webdevx@mail.ru>
2023-08-23 15:40:38 +0300

    added fonts, header

--- gulpfile.js
+++ gulpfile.js
@@ -19,7 +19,7 @@ function browsersync() {
 
 function styles() {
   return src("app/scss/style.scss")
-    .pipe(scss({ outputStyle: "compressed" }))
+    .pipe(scss({ outputStyle: "expanded" }))
     .pipe(concat("style.min.css"))
     .pipe(
       autoprefixer({
